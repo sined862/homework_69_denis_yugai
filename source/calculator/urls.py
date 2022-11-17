c4ec4985891508ec2f059b487c162130d463e99c
Yugai Denis <yugai_denis@mail.ru>
2022-11-17 14:36:48 +0600

    Установка Django и выполнение задания 1

---
+++ source/calculator/urls.py
@@ -0,0 +1,10 @@
+﻿from django.urls import path
+from calculator.views import index_view, add_view, subtract_view, multiply_view, divide_view
+
+urlpatterns = [
+    path('', index_view, name='index'),
+    path('add/', add_view, name='add'),
+    path('subtract/', subtract_view, name='subtract'),
+    path('multiply/', multiply_view, name='multiply'),
+    path('divide/', divide_view, name='divide')
+]
---
+++ source/calculator/views.py
@@ -0,0 +1,111 @@
+from django.shortcuts import render
+import json
+from django.http import JsonResponse, HttpResponse
+
+numbers = {
+        "A": 555,
+        "B": 0
+    }
+a = numbers.get("A")
+b = numbers.get("B")
+
+def index_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return render(request, 'index.html')
+
+
+def is_number(number):
+    try:
+        float(number)
+        return True
+    except ValueError:
+        return False
+
+
+def add_view(request, *args, **kwargs):
+
+    if is_number(a) or is_number(b):
+        sum = a + b
+        answer = {
+            "answer": sum
+        }
+        if request.body:
+            answer['content'] = json.loads(request.body)
+        return JsonResponse(answer)
+    else:
+        answer = {
+            "error": "Not number"
+        }
+        HttpResponse.status_code = 400
+        if request.body:
+            answer['content'] = json.loads(request.body)
+        return JsonResponse(answer)
+
+
+def subtract_view(request, *args, **kwargs):
+
+    if is_number(a) or is_number(b):
+        sum = a - b
+        answer = {
+            "answer": sum
+        }
+        if request.body:
+            answer['content'] = json.loads(request.body)
+        return JsonResponse(answer)
+    else:
+        answer = {
+            "error": "Not number"
+        }
+        HttpResponse.status_code = 400
+        if request.body:
+            answer['content'] = json.loads(request.body)
+        return JsonResponse(answer)
+
+
+def multiply_view(request, *args, **kwargs):
+
+    if is_number(a) or is_number(b):
+        sum = a * b
+        answer = {
+            "answer": sum
+        }
+        if request.body:
+            answer['content'] = json.loads(request.body)
+        return JsonResponse(answer)
+    else:
+        answer = {
+            "error": "Not number"
+        }
+        HttpResponse.status_code = 400
+        if request.body:
+            answer['content'] = json.loads(request.body)
+        return JsonResponse(answer)
+
+
+def divide_view(request, *args, **kwargs):
+    
+    if is_number(a) or is_number(b):
+        if b != 0:
+            sum = a / b
+            answer = {
+                "answer": sum
+            }
+            if request.body:
+                answer['content'] = json.loads(request.body)
+            return JsonResponse(answer)
+        else:
+            answer = {
+            "error": "Division by zero!"
+        }
+        HttpResponse.status_code = 400
+        if request.body:
+            answer['content'] = json.loads(request.body)
+        return JsonResponse(answer)
+    else:
+        answer = {
+            "error": "Not number"
+        }
+        HttpResponse.status_code = 400
+        if request.body:
+            answer['content'] = json.loads(request.body)
+        return JsonResponse(answer)
